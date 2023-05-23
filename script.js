b545c02a4099db625c6ef8e162433f3744190c4a
sachinsherekar <130824306+sachinsherekar@users.noreply.github.com>
2023-05-23 17:44:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 let container=document.createElement("div");
-container.classList.add="container";
+container.classList.add=("container");
 let line=document.createElement('div');
 line.id="line";
 container.append(line);
